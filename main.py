a1692028d9b5bdd4730880c0704944c2561c6e9e
Brendan Smith <brendan.smith.93@gmail.com>
2014-11-18 17:42:02 -0600

    Pull scrapers to separate class file.

---
+++ main.py
@@ -0,0 +1,10 @@
+from Scrapers import MajorHoldersScraper
+
+
+def main():
+    scraper = MajorHoldersScraper()
+    names = scraper.scrape('AAPL')
+    print(names)
+
+if __name__ == '__main__':
+    main()
