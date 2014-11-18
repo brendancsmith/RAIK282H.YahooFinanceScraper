b933ad7b9aec0d1e06cf5d3d08081ac2c54db766
Brendan Smith <brendan.smith.93@gmail.com>
2014-11-18 17:16:49 -0600

    Create some functions, yay.

--- scrape.py
+++ scrape.py
@@ -1,18 +1,43 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-optionsUrl = 'https://finance.yahoo.com/q/mh?s=AAPL+Major+Holders'
-optionsPage = urlopen(optionsUrl)
+baseUrl = 'https://finance.yahoo.com/q'
 
-soup = BeautifulSoup(optionsPage)
 
-table = soup.find('table', attrs={'class': 'yfnc_tableout1'}).find('table')
-#cells = table.findAll('td', attrs={'class': 'yfnc_tabledata1'})
-#print([cell.parent for cell in cells])
+def construct_url(symbol, queryAcronym):
+    url = baseUrl
 
-rows = table.findAll('tr')
-rows = [row for row in rows
-        if row.find('td', attrs={'class': 'yfnc_tabledata1'})]
-names = [row.find('td').text for row in rows]
+    if(queryAcronym):  # add acronym for page type (ie. 'mh' for Major Holders)
+        url += '/{}'.format(queryAcronym)
 
-print(names)
+    url += '?s={}'.format(symbol)  # add symbol parameter
+
+    return url
+
+
+def create_MH_soup(symbol):
+    url = construct_url(symbol, 'mh')
+    page = urlopen(url)
+    soup = BeautifulSoup(page)
+
+    return soup
+
+
+def extract_major_holders(soup):
+    # get major holders table
+    table = soup.find('table', attrs={'class': 'yfnc_tableout1'}).find('table')
+
+    rows = table.findAll('tr')  # includes header, we'll filter it out
+    hasDataCell = lambda x: x.find('td', attrs={'class': 'yfnc_tabledata1'})
+    rows = filter(hasDataCell, rows)
+
+    names = [row.find('td').text for row in rows]  # content of first column
+    return names
+
+
+def main():
+    names = extract_major_holders(create_MH_soup('AAPL'))
+    print(names)
+
+if __name__ == '__main__':
+    main()
