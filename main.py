84ee7fd23fd8ad15292797ddf05fede362cc0a5c
Brendan Smith <brendan.smith.93@gmail.com>
2014-11-18 18:03:49 -0600

    Retrieve S&P 500 symbols.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python2
 
 from Scrapers import MajorHoldersScraper
+import finsymbols
+
+
+def get_sp500_symbols():
+    return [stock['symbol'] for stock in finsymbols.get_sp500_symbols()]
 
 
 def main():
+    print(len(get_sp500_symbols()))
+
     scraper = MajorHoldersScraper()
     names = scraper.scrape('AAPL')
     print(names)
